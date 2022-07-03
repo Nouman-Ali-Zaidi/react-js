536248b8f63fa900da951cd914d66a6ba5623034
Nouman <noumanali.cs@gmail.com>
2022-07-03 22:35:10 +0500

    Adding my project to git hub

---
+++ src/components/Menuapi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/maggi.jpg",
+        name: "maggi",
+        category: "breakfast",
+        price: "120Rs",
+        description:
+            "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+
+    {
+        id: 2,
+        image: "images/fries.jpg",
+        name: "fries",
+        category: "evening",
+        price: "100Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 3,
+        image: "images/corn.jpg",
+        name: "corn",
+        category: "breakfast",
+        price: "30Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 4,
+        image: "../images/cholay.jpg",
+        name: "cholay",
+        category: "lunch",
+        price: "150Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 5,
+        image: "../images/pizza.webp",
+        name: "pizza",
+        category: "evening",
+        price: "450Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 6,
+        image: "../images/Beryani-1.jpg",
+        name: "beryani",
+        category: "dinner",
+        price: "320Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 7,
+        image: "../images/zarda.jpg",
+        name: "zarda",
+        category: "dinner",
+        price: "150Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 8,
+        image: "../images/dal-chawal.jpg",
+        name: "dal-chawal",
+        category: "lunch",
+        price: "170Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "samaso",
+        category: "evening",
+        price: "30Rs",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+];
+
+export default Menu;
---
+++ src/components/Resturant.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useState } from 'react';
+import Menu from "./Menuapi";
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [
+    ...new Set(
+        Menu.map((curElem) => {
+            return curElem.category;
+        })
+    ),
+    "All"
+]
+
+export const Resturant = () => {
+
+    const [menuData, setMenuData] = useState(Menu);
+    const [menuList, setMenuList] = useState(uniqueList);
+    const filterItem = (category) => {
+        if (category === "All") {
+            setMenuData(Menu)
+            return;
+        }
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category === category;
+        });
+        setMenuData(updatedList)
+    }
+    return (
+        <>
+            <Navbar filterItem={filterItem} menuList={menuList} />
+            <MenuCard menuData={menuData} />
+
+        </>
+    )
+}
